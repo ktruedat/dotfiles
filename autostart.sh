f98c6098a342ae1fdfc93c6125f7912ecd84bb28
Oglo12 <ogloooglooagla@gmail.com>
2023-06-01 20:04:08 -0500

    More stuff.

--- autostart.sh
+++ autostart.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
+
+
 source ~/.config/hypr/lib.sh
 
-set-wallpaper ~/.config/hypr/wallpaper.png
+
+
+run_hook pre &
+
+set_wallpaper ~/.config/hypr/wallpaper.png
+
 ~/.config/hypr/waybar/start
+
+nm-applet &
+blueman-applet &
+
+lxsession &
+
+brightnessctl --restore
+
+run_hook post &
--- lib.sh
+++ lib.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-set-wallpaper () {
+
+
+set_wallpaper () {
 	killall swaybg
 	sleep 0.05
 	swaybg -i "$1" -o '*' &
 	disown
+}
+
+run_hook () {
+	$HOME/.hyprland_rice/autostart_$1
 }
