d0e5a020ec76be8ba3a0ce5e504cb6cee70ac434
Oglo12 <ogloooglooagla@gmail.com>
2023-07-10 20:29:57 -0500

    Flameshot killing hotfix.

--- autostart.sh
+++ autostart.sh
@@ -8,9 +8,6 @@ source ~/.config/hypr/lib.sh
 
 run_hook pre &
 
-killall flameshot
-pkill flameshot
-
 swww init
 
 set_wallpaper ~/.config/hypr/wallpaper.png
@@ -27,5 +24,7 @@ lxsession &
 brightnessctl --restore
 
 eval "sleep 2; hyprctl reload" &
+
+eval "sleep 2; killall flameshot; pkill flameshot" &
 
 run_hook post &
