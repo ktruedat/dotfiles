52d9d3fdf271ba5d9a71f1caed0b1ed3eb6e48ec
Oglo12 <ogloooglooagla@gmail.com>
2023-10-19 16:51:57 -0500

    Don't save brightness when using keybind.

--- scripts/change_brightness.sh
+++ scripts/change_brightness.sh
@@ -20,5 +20,3 @@ elif [[ $b_mode == "down" ]]; then
 else
   notify-send "Hyprland Script (Brightness)" "You seem to be using this script wrong somewhere in your configuration!" -u critical
 fi
-
-brightnessctl --save
