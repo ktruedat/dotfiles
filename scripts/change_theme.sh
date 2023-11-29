1a8c03cc176c16c0bc03879e2271f74c031b213c
Oglo12 <jacksonnovak@proton.me>
2023-11-29 13:19:49 -0600

    Fixed category bug.

--- scripts/change_theme.sh
+++ scripts/change_theme.sh
@@ -6,19 +6,21 @@
 
 rm ~/.cache/hyprland_rice/theme_list.txt > /dev/null 2>&1
 
-theme_categ=$(echo -e "All\nBuilt-In\nCustom" | rofi -dmenu -p " 󰉼  Categories ")
-
-echo "Chosen Category: $theme_categ"
-
-if [[ $theme_categ == "All" ]]; then
-  cat $HOME/.config/hypr/themes/themes.txt $HOME/.hyprland_rice/themes.txt > $HOME/.cache/hyprland_rice/theme_list.txt
-elif [[ $theme_categ == "Built-In" ]]; then
-  cat $HOME/.config/hypr/themes/themes.txt > $HOME/.cache/hyprland_rice/theme_list.txt
-elif [[ $theme_categ == "Custom" ]]; then
-  cat $HOME/.hyprland_rice/themes.txt > $HOME/.cache/hyprland_rice/theme_list.txt
-else
-  echo "Invalid category."
-  exit 1
+if [[ "$1" == "" ]]; then
+  theme_categ=$(echo -e "All\nBuilt-In\nCustom" | rofi -dmenu -p " 󰉼  Categories ")
+  
+  echo "Chosen Category: $theme_categ"
+  
+  if [[ $theme_categ == "All" ]]; then
+    cat $HOME/.config/hypr/themes/themes.txt $HOME/.hyprland_rice/themes.txt > $HOME/.cache/hyprland_rice/theme_list.txt
+  elif [[ $theme_categ == "Built-In" ]]; then
+    cat $HOME/.config/hypr/themes/themes.txt > $HOME/.cache/hyprland_rice/theme_list.txt
+  elif [[ $theme_categ == "Custom" ]]; then
+    cat $HOME/.hyprland_rice/themes.txt > $HOME/.cache/hyprland_rice/theme_list.txt
+  else
+    echo "Invalid category."
+    exit 1
+  fi
 fi
 
 key_to_value () {
